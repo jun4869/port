3adbf183c401a1e6d7fc2bbc8361cf6a0956428a
jun4869 <jun2194k@gmail.com>
2022-01-12 08:53:46 +0900

    view and validates

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,5 @@
 class Board < ApplicationRecord
   has_many :posts,dependent: :destroy
+  validates :author, presence: true,length: { maximum: 10}
+  validates :title, presence: true,length: { maximum: 30}
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :board
+  validates :contributor, presence: true,length: { maximum: 10}
 end
