e48560581804aaf3354fb7d9198865a45df0b6ee
jun4869 <jun2194k@gmail.com>
2022-01-12 01:13:13 +0900

    board view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'boards/index'
-  get 'boards/new'
-  get 'boards/create'
-  get 'boards/show'
+  root to: 'boards#index'
+  resources :boards, only: %i[index new create show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
